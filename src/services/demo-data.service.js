bbf1c31f640fa6003bdc051c990469a143c1f003
Avi Iskaov <avitech1242@gmail.com>
2022-08-30 09:07:32 +0300

    adding demo-data

---
+++ src/services/demo-data.service.js
@@ -0,0 +1,360 @@
+export const data = [
+    {
+        manufacturer: "HYUNDAI",
+        model: "IONIQ",
+        type: "family",
+        price: "199900",
+        height: 1647,
+        trunk: 531,
+        horsePower: 217,
+        isGearAuto: true,
+        safty: 7,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: true,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "KONA EV",
+        type: "family",
+        price: "148900",
+        height: 1570,
+        trunk: 332,
+        horsePower: 136,
+        isGearAuto: true,
+        safty: 7,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "ELANTRA Hybrid",
+        type: "executive",
+        price: "152900",
+        height: 1420,
+        trunk: 474,
+        motor: 1.6,
+        gasType: "benzin",
+        horsePower: 105,
+        safty: 6,
+        gasUsage: 4.4,
+        isGearAuto: true,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: true,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "ELANTRA VENUE",
+        type: "suv",
+        price: "113900",
+        motor: 1.6,
+        gasType: "benzin",
+        horsePower: 123,
+        height: 1892,
+        trunk: 355,
+        safty: 6,
+        gasUsage: 6.6,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: false,
+            isElectricHandBreaks: false
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "TUCSON",
+        type: "family",
+        price: "166900",
+        motor: 2,
+        gasType: "benzin",
+        horsePower: 156,
+        height: 1650,
+        trunk: 515,
+        safty: 6,
+        gasUsage: 8.2,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: true,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "i10",
+        type: "mini",
+        price: "82900",
+        motor: 1.2,
+        gasType: "benzin",
+        horsePower: 84,
+        height: 1480,
+        trunk: 252,
+        safty: 6,
+        gasUsage: 4.2,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: false,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: false,
+            isStartButton: false,
+            isElectricHandBreaks: false
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "KONA Hybrid",
+        type: "family",
+        price: "149900",
+        motor: 1.6,
+        gasType: "benzin",
+        horsePower: 139,
+        height: 1550,
+        trunk: 374,
+        safty: 6,
+        gasUsage: 3.9,
+        isGearAuto: true,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "KONA",
+        type: "family",
+        price: "131900",
+        motor: 1.6,
+        gasType: "benzin",
+        horsePower: 198,
+        height: 1550,
+        trunk: 374,
+        safty: 6,
+        gasUsage: 6.6,
+        isGearAuto: true,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: false
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "SONATA Hybrid",
+        type: "executive",
+        price: "201900",
+        motor: 2,
+        gasType: "benzin",
+        horsePower: 192,
+        height: 1445,
+        trunk: 510,
+        safty: 7,
+        gasUsage: 5,
+        isGearAuto: true,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: true,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "SONATA Turbo",
+        type: "executive",
+        price: "189900",
+        motor: 1.6,
+        gasType: "benzin",
+        horsePower: 180,
+        height: 1445,
+        trunk: 510,
+        safty: 7,
+        gasUsage: 7.6,
+        isGearAuto: true,
+        isElectric: true,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: true,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "i20",
+        type: "mini",
+        price: "108900",
+        motor: 1,
+        gasType: "benzin",
+        horsePower: 100,
+        height: 1450,
+        trunk: 352,
+        safty: 6,
+        gasUsage: 5.7,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: false,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: false,
+            isElectricHandBreaks: false
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "Sante Fe",
+        type: "SUV",
+        price: "269900",
+        motor: 2.2,
+        gasType: "dizel",
+        horsePower: 202,
+        height: 1710,
+        trunk: 571,
+        safty: 8,
+        gasUsage: 6.3,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: true,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: false,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "PALISADE",
+        type: "SUV",
+        price: "319900",
+        motor: 3.8,
+        gasType: "benzin",
+        horsePower: 291,
+        height: 1750,
+        trunk: 311,
+        safty: 8,
+        gasUsage: 11.1,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: true,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: true,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: true
+        }
+
+    },
+    {
+        manufacturer: "HYUNDAI",
+        model: "BAYON",
+        type: "family",
+        price: "123900",
+        motor: 1,
+        gasType: "benzin",
+        horsePower: 100,
+        height: 1500,
+        trunk: 373,
+        safty: 6,
+        gasUsage: 5.5,
+        isGearAuto: true,
+        isElectric: false,
+        features: {
+            isMagnezioumWheels: true,
+            isPanoramicRoof: false,
+            isMultimediaSystem: true,
+            isCarPlay: true,
+            isLeatherCovers: true,
+            isDigitalClocks: true,
+            isStartButton: true,
+            isElectricHandBreaks: false
+        }
+
+    },
+]
