37d3bb6643b0ca2d2c362d109af4840a2ab86a07
Avi Iskaov <avitech1242@gmail.com>
2022-09-03 19:15:42 +0300

    Change form logic to accept int types of input

--- src/cmps/edit/car-edit.jsx
+++ src/cmps/edit/car-edit.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { carService } from '../../services/car.service'
 import { carEditFields } from '../../consts/form-fields'
+import { DynamicInput } from './dynamic-input'
 
 export const CarEdit = () => {
   const params = useParams()
@@ -20,56 +21,13 @@ export const CarEdit = () => {
 
   const handleChange = (ev) => {
     const field = ev.target.id
-    const value = typeof +ev.target.value === 'number' ? +ev.target.value : ev.target.value
+    const value = isNaN(ev.target.value) || typeof ev.target.value === 'boolean' ? ev.target.value : +ev.target.value
     setCar((prevState) => ({ ...prevState, [field]: value }))
   }
 
   const handleFeatureChange = (ev) => {
     const field = ev.target.value
     setCar((prevState) => ({ ...prevState, features: { ...prevState.features, [field]: !prevState.features[field] } }))
-  }
-
-  const getDynamicInput = (field) => {
-    const props = { key: field.name, id: field.name, value: car[field.name], onChange: handleChange }
-    switch (field.type) {
-      case 'dropdown':
-        return (
-          <select {...props}>
-            {field.options.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.he}
-              </option>
-            ))}
-          </select>
-        )
-      case 'number':
-        return <input type="number" {...props} />
-      case 'range':
-        return <input type="range" {...props} min={field.range.min} max={field.range.max} />
-      case 'multi-check-select':
-        return (
-          <div className="multi-select-container">
-            {field.options.map((opt) => (
-              <label key={opt.value}>
-                {opt.he}
-                <input type="checkbox" value={opt.value} onChange={handleFeatureChange} checked={car.features[opt.value]} />
-              </label>
-            ))}
-          </div>
-        )
-      case 'boolean':
-        return (
-          <div className="bool-btns-container">
-            {field.options.map((opt, idx) => (
-              <button key={idx} type="button" onClick={() => handleChange({ target: { id: field.name, value: opt.value } })}>
-                {opt.he}
-              </button>
-            ))}
-          </div>
-        )
-      default:
-        break
-    }
   }
 
   return (
@@ -80,7 +38,7 @@ export const CarEdit = () => {
             return (
               <div key={field.name} className="field-container">
                 <label htmlFor={field.name}>{field.label.he}</label>
-                {getDynamicInput(field)}
+                <DynamicInput field={field} handleChange={handleChange} handleFeatureChange={handleFeatureChange} car={car} />
               </div>
             )
           })}
---
+++ src/cmps/edit/dynamic-input.jsx
@@ -0,0 +1,42 @@
+export const DynamicInput = ({ field, handleChange, handleFeatureChange, car }) => {
+  const props = { key: field.name, id: field.name, value: car[field.name], onChange: handleChange }
+  switch (field.type) {
+    case 'dropdown':
+      return (
+        <select {...props}>
+          {field.options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.he}
+            </option>
+          ))}
+        </select>
+      )
+    case 'number':
+      return <input type="number" {...props} />
+    case 'range':
+      return <input type="range" {...props} min={field.range.min} max={field.range.max} />
+    case 'multi-check-select':
+      return (
+        <div className="multi-select-container">
+          {field.options.map((opt) => (
+            <label key={opt.value}>
+              {opt.he}
+              <input type="checkbox" value={opt.value} onChange={handleFeatureChange} checked={car.features[opt.value]} />
+            </label>
+          ))}
+        </div>
+      )
+    case 'boolean':
+      return (
+        <div className="bool-btns-container">
+          {field.options.map((opt, idx) => (
+            <button key={idx} type="button" onClick={() => handleChange({ target: { id: field.name, value: opt.value } })}>
+              {opt.he}
+            </button>
+          ))}
+        </div>
+      )
+    default:
+      break
+  }
+}
