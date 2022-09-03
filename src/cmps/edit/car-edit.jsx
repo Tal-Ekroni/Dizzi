5d1ab9b5aaa639d1469d1fba0f0b6f1e10dc0d2e
Avi Iskaov <avitech1242@gmail.com>
2022-09-03 18:58:10 +0300

    Add logic to changing fields in form

--- src/cmps/edit/car-edit.jsx
+++ src/cmps/edit/car-edit.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { carService } from '../../services/car.service'
 import { carEditFields } from '../../consts/form-fields'
+
 export const CarEdit = () => {
   const params = useParams()
   const navigate = useNavigate()
@@ -13,13 +14,23 @@ export const CarEdit = () => {
 
     carService.getById(carId).then((carToEdit) => {
       if (!carToEdit) navigate('/')
-      console.log(carToEdit)
       setCar(carToEdit)
     })
   }, [])
 
+  const handleChange = (ev) => {
+    const field = ev.target.id
+    const value = typeof +ev.target.value === 'number' ? +ev.target.value : ev.target.value
+    setCar((prevState) => ({ ...prevState, [field]: value }))
+  }
+
+  const handleFeatureChange = (ev) => {
+    const field = ev.target.value
+    setCar((prevState) => ({ ...prevState, features: { ...prevState.features, [field]: !prevState.features[field] } }))
+  }
+
   const getDynamicInput = (field) => {
-    const props = { key: field.name, id: field.name, value: car[field.name] }
+    const props = { key: field.name, id: field.name, value: car[field.name], onChange: handleChange }
     switch (field.type) {
       case 'dropdown':
         return (
@@ -39,10 +50,20 @@ export const CarEdit = () => {
         return (
           <div className="multi-select-container">
             {field.options.map((opt) => (
-              <label>
+              <label key={opt.value}>
                 {opt.he}
-                <input type="checkbox" key={opt.value} value={opt.value} checked={car.features[opt.value]} />
+                <input type="checkbox" value={opt.value} onChange={handleFeatureChange} checked={car.features[opt.value]} />
               </label>
+            ))}
+          </div>
+        )
+      case 'boolean':
+        return (
+          <div className="bool-btns-container">
+            {field.options.map((opt, idx) => (
+              <button key={idx} type="button" onClick={() => handleChange({ target: { id: field.name, value: opt.value } })}>
+                {opt.he}
+              </button>
             ))}
           </div>
         )
