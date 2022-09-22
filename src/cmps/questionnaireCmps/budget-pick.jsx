669a45ef624e997ee7c3858d4226276b60e55446
Avi Iskaov <avitech1242@gmail.com>
2022-09-22 12:39:22 +0300

    Add V-circle above chosen option in questionnaire

--- src/cmps/questionnaireCmps/budget-pick.jsx
+++ src/cmps/questionnaireCmps/budget-pick.jsx
@@ -1,44 +1,40 @@
-import { Slider } from "@mui/material";
-import { useState } from "react";
+import { Slider } from '@mui/material'
+import { useState } from 'react'
+import { utilService } from '../../services/util.service'
 
 function valuetext(value) {
-  return `${value}°C`;
+  return utilService.priceFormat(value)
 }
-const minDistance = 10;
+const minDistance = 10
 
-export default function BudgetPick({
-  questionnaire,
-  setQuestionnaire,
-  currentSection,
-  setCurrentSection,
-}) {
-  const [value1, setValue1] = useState([0, 20000]);
-  const [value2, setValue2] = useState([0, 1000]);
+export default function BudgetPick({ questionnaire, setQuestionnaire, currentSection, setCurrentSection }) {
+  const [value1, setValue1] = useState([0, 20000])
+  const [value2, setValue2] = useState([0, 1000])
   const handleChange1 = (event, newValue, activeThumb) => {
     if (!Array.isArray(newValue)) {
-      return;
+      return
     }
     if (questionnaire.isFullPayment) {
       if (activeThumb === 0) {
-        setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+        setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]])
       } else {
-        setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+        setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)])
       }
     } else {
       if (activeThumb === 0) {
-        setValue2([Math.min(newValue[0], value2[1] - minDistance), value2[1]]);
+        setValue2([Math.min(newValue[0], value2[1] - minDistance), value2[1]])
       } else {
-        setValue2([value2[0], Math.max(newValue[1], value2[0] + minDistance)]);
+        setValue2([value2[0], Math.max(newValue[1], value2[0] + minDistance)])
       }
     }
-  };
+  }
 
   const onChooseOption = () => {
     setQuestionnaire({
       ...questionnaire,
       isFullPayment: !questionnaire.isFullPayment,
-    });
-  };
+    })
+  }
   return (
     <div className="budget-pick">
       <h1>מה התקציב שלך?</h1>
@@ -47,34 +43,30 @@ export default function BudgetPick({
       <div className="switch-container">
         <span>מחיר מלא</span>
         <label className="switch">
-          <input
-            type="checkbox"
-            defaultChecked={questionnaire.isFullPayment}
-            onChange={onChooseOption}
-          />
+          <input type="checkbox" defaultChecked={questionnaire.isFullPayment} onChange={onChooseOption} />
           <span className="slider round"></span>
         </label>
         <span>תשלום חודשי</span>
       </div>
       <div className="slider-container">
         <Slider
-          getAriaLabel={() => "Minimum distance"}
+          getAriaLabel={() => 'Minimum distance'}
           value={questionnaire.isFullPayment ? value1 : value2}
           onChange={handleChange1}
           valueLabelDisplay="on"
           sx={{
             width: 600,
-            padding: "3px",
-            color: "white",
-            "& .MuiSlider-rail": {
-              color: "white",
+            padding: '3px',
+            color: 'white',
+            '& .MuiSlider-rail': {
+              color: 'white',
             },
-            "& .MuiSlider-valueLabelOpen": {
-              backgroundColor: "white",
-              color: "#48966c",
-              borderRadius: "20px",
-              fontSize: "17px",
-              fontWeight: "600",
+            '& .MuiSlider-valueLabelOpen': {
+              backgroundColor: 'white',
+              color: '#48966c',
+              borderRadius: '20px',
+              fontSize: '17px',
+              fontWeight: '600',
             },
           }}
           getAriaValueText={valuetext}
@@ -84,11 +76,11 @@ export default function BudgetPick({
       </div>
       <button
         onClick={() => {
-          setCurrentSection("CarType");
+          setCurrentSection('CarType')
         }}
       >
         המשך
       </button>
     </div>
-  );
+  )
 }
